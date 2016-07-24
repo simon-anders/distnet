f416b04135eb4a44be70ce8ce6e95f905f218be9
simon-anders <sanders@fs.tum.de>
2016-07-24 15:54:58 +0300

    put sigmoidColorSlider in its own file

---
+++ inst/htmlwidgets/lib/distnet/sigmoidColorSlider.js
@@ -0,0 +1,68 @@
+"use strict";
+
+function sigmoidColorSlider( divElement, maxVal, minColor, maxColor ) {
+
+  var obj = {}
+
+  obj.maxVal = maxVal===undefined ? 1 : maxVal;
+  obj.minColor = minColor===undefined ? "darkblue" : maxVal;
+  obj.maxColor = maxColor===undefined ? "white" : maxVal;
+
+  var table = divElement.append("table")
+    .style( "width", "100%" )
+    .style( "table-layout", "fixed" );
+  var td1 = table.append("tr").append("td");
+  var td2 = table.append("tr").append("td");
+  var td3 = table.append("tr").append("td");
+  
+  var colorBarContainer = td1.append( "svg" )
+    .attr( "width", "100%")
+    .style( "height", "30px" );
+  var theColorBar = colorBar( colorBarContainer );
+
+  var threshSlider = d3.slider()
+    .axis( true )
+    .max( obj.maxVal )
+    .value( obj.maxVal / 4 );
+  td2.call( threshSlider )
+
+  var slopeSlider = d3.slider()
+    .max( 200 / threshSlider.max() )
+    .value( 50 / threshSlider.max() );
+  td3.call( slopeSlider )      
+
+  obj.dispatch = d3.dispatch( "change" );
+
+  obj.on = function() { obj.dispatch.on.apply( obj.dispatch, arguments ); }
+
+  obj.update = function( ) {
+    
+    obj.scale = function( x ) { return sigmoid( x, threshSlider.value(), -slopeSlider.value(), .05 ) }; 
+    
+    var linColorScale = d3.scale.linear()
+      .domain( [ 1, 0 ] )
+      .range( [ obj.minColor, obj.maxColor ] );
+
+    obj.colorScale = function(x) { return linColorScale( obj.scale( x ) ) }
+
+    theColorBar.scale = function(x) { return obj.colorScale( x * threshSlider.max() ) };
+
+    theColorBar.update();
+
+  }
+
+  threshSlider.on( "slide", function() { obj.update(); obj.dispatch.change(); } );
+  slopeSlider.on( "slide", function() { obj.update(); obj.dispatch.change(); } );
+
+  obj.update();
+
+  return obj;
+
+}
+
+
+// The sigmoid function:
+function sigmoid( x, threshold, slope, threshVal ) {
+   var midpoint = threshold + Math.log( 1/threshVal - 1 ) / slope
+   return 1 / ( 1 + Math.exp( -slope * ( x - midpoint ) ) )
+}
