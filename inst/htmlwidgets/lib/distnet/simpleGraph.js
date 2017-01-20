af77b29488b810a6dbd2213a0fa651fc4c01c3a0
simon-anders <sanders@fs.tum.de>
2017-01-20 15:36:35 +0100

    paced redrawing now works

--- inst/htmlwidgets/lib/distnet/simpleGraph.js
+++ inst/htmlwidgets/lib/distnet/simpleGraph.js
@@ -2,10 +2,18 @@
 
 function simpleGraph( id, chart ) {
 
-  if(chart === undefined)
+  if(chart === undefined) {
     chart = d3.axisChartBase();
+  }
   if(id === undefined)
     id = "layer" + chart.layers.length;
+
+  var inherited_put_static_content = chart.put_static_content;
+  chart.put_static_content = function( element ) {
+    inherited_put_static_content( element );
+    // Remove axes:
+    element.selectAll( "g.axis" ).style( "display", "none" );
+  }
 
   var layer = chart.add_layer(id)
     .add_property("x")
@@ -22,8 +30,6 @@ function simpleGraph( id, chart ) {
   
   // Set default for dataIds, namely to return numbers accoring to numPoints
   layer.dataIds( function() { return d3.range( layer.get_npoints() ) } );
-
-  layer.update_not_yet_called = true;
   
   layer.layerDomainX(function() {
     return d3.extent( layer.get_dataIds(), function(k) { return layer.get_x(k) } )
@@ -32,15 +38,22 @@ function simpleGraph( id, chart ) {
     return d3.extent( layer.get_dataIds(), function(k) { return layer.get_y(k) } )
   });
 
+  layer.update_not_yet_called = true;
+
   layer.update = function() {
 
     if( layer.update_not_yet_called ) {
-      layer.update_not_yet_called = false;   
+      // layer.update_not_yet_called is set to false further below
       layer.g = layer.chart.svg.append("g")
         .attr("class", "chart_g");
     } 
+
+    var transition_or_selection = 
+      layer.get_transitionDuration() > 0 ?
+        function( sel ) { return sel.transition( chart.transition ); } :
+        function( sel ) { return sel; };
     
-    layer.g.transition(layer.chart.transition)
+    layer.g.call( transition_or_selection )
       .attr("transform", "translate(" + 
         layer.get_margin().left + ", " +
         layer.get_margin().top + ")");
@@ -54,6 +67,16 @@ function simpleGraph( id, chart ) {
         edges.push( [ vertices[i], vertices[j] ] )
       }
     }
+    
+    var sel = layer.g.selectAll( ".graph_edge" )
+      .data( edges )
+      .call( function(x) { layer.get_edge_dresser(x) } );
+
+    if( !layer.update_not_yet_called ) {
+      return;
+    }
+    layer.update_not_yet_called = false;   
+      
 
     var sel = layer.g.selectAll( ".graph_edge" )
       .data( edges );
@@ -62,27 +85,26 @@ function simpleGraph( id, chart ) {
     sel.enter().append( "line" )
       .attr( "class", "graph_edge" )
     .merge( sel )
-      .transition(layer.chart.transition)
+      .call( transition_or_selection )
         .attr( "x1", function(d) { return layer.chart.axes.scale_x( layer.get_x( d[0] ) ) } )
         .attr( "y1", function(d) { return layer.chart.axes.scale_y( layer.get_y( d[0] ) ) } )
         .attr( "x2", function(d) { return layer.chart.axes.scale_x( layer.get_x( d[1] ) ) } )
         .attr( "y2", function(d) { return layer.chart.axes.scale_y( layer.get_y( d[1] ) ) } )
-        //.style( "display", function(d) { return layer.get_edge_present(d[0],d[1]) ? "yes" : "none" ; } )
+        .style( "display", function(d) { return layer.get_edge_present(d[0],d[1]) ? "initial" : "none" ; } )
         .call( function(x) { layer.get_edge_dresser(x) } )
 
-    var sel = layer.g.selectAll( ".data_point" )
+    var sel = layer.g.selectAll( ".graph_vertex" )
       .data( vertices );
     sel.exit()
       .remove();  
     sel.enter().append( "circle" )
-      .attr( "class", "data_point" )
+      .attr( "class", "graph_vertex" )
       .attr( "r", function(d) {return layer.get_size(d)} )
     .merge( sel )
-      .transition(layer.chart.transition)
+      .call( transition_or_selection )
         .attr( "cx", function(d) { return layer.chart.axes.scale_x( layer.get_x(d) ) } )
         .attr( "cy", function(d) { return layer.chart.axes.scale_y( layer.get_y(d) ) } )
         .attr( "style", function(d) { return layer.get_vertex_style(d) } );
-;
 
     return layer;
   };
